d24f8f274aa0812b533efac186159281c35de851
Alberto Ribelles <alberto.ribelles@gmail.com>
2020-11-18 19:18:55 +0100

    digits source file cleaned

--- digits.cs
+++ digits.cs
@@ -2,34 +2,34 @@ using System;
 
 class digits
 {
- static void Main()
- {
-	 //counters of number of digits a is for one digit, b is for 2 digits, c is for 3 digits and d is for more than 3 digits
-     int a = 0, b = 0, c = 0, d = 0;
-      Console.Write("Insert a number: ");
-      // read the number as string to check the word "end"
-      string e = Console.ReadLine();
-     while(e!="end"){
-int input = Convert.ToInt32(e);
-       if(input != 0)
-{
-       if(input/10 == 0)
-           a ++;  // it only has one digit
-       else 
-       if (input/100 == 0)
-		b ++;  // the number has 2 digits
-        else
-        if (input/1000 == 0)
-        c ++;  // the number has 3 digits
-        else
-           d++;  // the number has more than 3 digits
+    static void Main()
+    {
+        //counters for number depending on how many digits they have
+        int singleDigit = 0, doubleDigit = 0, tripleDigit = 0, multipleDigit = 0;
+        string number;
+        
+        Console.Write("Insert a number: ");
+        number = Console.ReadLine(); /*read the number as string to check the 
+        word "end"*/
+        
+        //while user does not introduce "end" we set different conditions
+        while (number!="end")
+        {
+            int input = Convert.ToInt32(number);
+            
+            if (input != 0)
+            {
+                if (input/10 == 0)
+                    singleDigit ++;  // the number only has 1 digit
+                else if (input/100 == 0)
+                    doubleDigit ++;  // the number has 2 digits
+                else if (input/1000 == 0)
+                    tripleDigit ++;  // the number has 3 digits
+                else
+                    multipleDigit++;  // the number has more than 3 digits
+            }
         }
-      e = Console.ReadLine();
+        Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", 
+        singleDigit, doubleDigit, tripleDigit, multipleDigit);
+    }
 }
-
-Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", a, b, c, d); 
-
-
- }
-}
-
