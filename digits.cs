6432744974d9e8936722656d993d019dd2a2237c
Alberto Ribelles <alberto.ribelles@gmail.com>
2020-11-18 19:52:52 +0100

    Some comments added to Digits source file

--- digits.cs
+++ digits.cs
@@ -4,7 +4,7 @@ class digits
 {
     static void Main()
     {
-        //counters for number depending on how many digits they have
+        //counters for numbers depending on how many digits they have
         int singleDigit = 0, doubleDigit = 0, tripleDigit = 0, multipleDigit = 0;
         string number;
         
@@ -15,10 +15,14 @@ class digits
         //while user does not introduce "end" we set different conditions
         while (number!="end")
         {
+            //We convert from string to int to work with the numbers
             int input = Convert.ToInt32(number);
             
             if (input != 0)
             {
+                /*To determine how many digits the number has we divide it by 10,
+                 * 100, 1000 and check the result, then increase the related 
+                 * counter*/
                 if (input/10 == 0)
                     singleDigit ++;
                 else if (input/100 == 0)
@@ -29,6 +33,7 @@ class digits
                     multipleDigit++;
             }
         }
+        //Finally we show the quantity of each one on screen
         Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", 
         singleDigit, doubleDigit, tripleDigit, multipleDigit);
     }
